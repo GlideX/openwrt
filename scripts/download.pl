9f5357f870e13a4475bfc4410f9575a752f8c3d8
arun <arun@glidebit.com>
2017-07-15 10:38:56 +0800

    Replaced OpenWrt mirror with GlideOS mirror server

--- scripts/download.pl
+++ scripts/download.pl
@@ -201,8 +201,8 @@ foreach my $mirror (@ARGV) {
 }
 
 #push @mirrors, 'http://mirror1.openwrt.org';
-push @mirrors, 'http://mirror2.openwrt.org/sources';
-push @mirrors, 'http://downloads.openwrt.org/sources';
+#push @mirrors, 'http://mirror2.openwrt.org/sources';
+push @mirrors, 'http://downloads.glideos.org/sources/';
 
 while (!$ok) {
 	my $mirror = shift @mirrors;
